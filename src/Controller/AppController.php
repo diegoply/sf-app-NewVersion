04818596aeb786b8a4c9065823ba68bd2197e8d0
Diego Ply <ply.dgo@gmail.com>
2024-03-04 19:42:26 +0100

    premier formulaire en cours

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,18 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/app', name: 'app')]
+    #[Route('/', name: 'app')]
     public function index(): Response
     {
-        $data = "Diego";
-        $age = 18;
-        $city = "Avignon";
-        $pays = "France";
+        
         return $this->render('app/index.html.twig', [
-            'data' => $data,
-            'age' => $age,
-            'city' => $city,
-            'pays' => $pays
+            
         ]);
     }
 }
---
+++ src/Controller/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout() : void
+    {
+        throw new Exception('Oups');
+    }
+}
