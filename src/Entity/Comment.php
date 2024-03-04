ad8e0a7de1bf7888fd99292d4930b2ae6eeead97
Diego Ply <ply.dgo@gmail.com>
2024-03-04 18:50:49 +0100

    definition table mais pas encore relations

---
+++ src/Entity/Comment.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $comment = null;
+
+    #[ORM\Column]
+    private ?int $raking = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getRaking(): ?int
+    {
+        return $this->raking;
+    }
+
+    public function setRaking(int $raking): static
+    {
+        $this->raking = $raking;
+
+        return $this;
+    }
+}
